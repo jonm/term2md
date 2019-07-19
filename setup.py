656697215fca9f5377b319097206419fec4101df
Jon Moore <jonm@jjmoore.net>
2019-07-19 19:34:53 -0400

    Integrate with `setuptools`
    * Reorganize for proper package structure.
    * Add `setup.py` configuration for packaging.
    * Update CI to invoke `pytest` via `setup.py`

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.1.0'
+
+def read(filename):
+    with open(filename) as f:
+        return f.read()
+
+setup(
+    name = 'term2md',
+    version = __version__,
+    description = 'convert terminal control characters to Markdown',
+    long_description = read('README.md'),
+    author = 'Jon Moore',
+    url = 'https://github.com/jonm/term2md',
+    packages = find_packages(),
+    include_package_data = True,
+    license='MIT',
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"]
+)
