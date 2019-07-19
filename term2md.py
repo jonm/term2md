2f3da1250d2f61da7f9017cf00ba182ad1d1d35a
Jon Moore <jonm@jjmoore.net>
2019-07-19 16:30:19 -0400

    Core working version.

---
+++ term2md.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python3
+
+import re
+import sys
+
+RESET = "\x1b[0m"
+BOLD = "\x1b[1m"
+RED = "\x1b[31m"
+GREEN = "\x1b[32m"
+
+def modes_in_line(parts, in_mode):
+    modes = {}
+    cur_mode = in_mode
+    for part in parts:
+        if part in (RESET, BOLD, RED, GREEN):
+            cur_mode = part
+        elif part.strip() != "":
+            modes[cur_mode] = True
+    return modes
+
+def non_control(parts):
+    return filter(lambda p: p not in (RESET, BOLD, RED, GREEN), parts)
+
+def leading(parts, in_mode, rg):
+    modes = modes_in_line(parts, in_mode)
+    if len(modes) != 2 or rg not in modes or RESET not in modes:
+        return False
+
+    cur_mode = in_mode
+    state = None
+    for part in parts:
+        if part in (rg, RESET):
+            cur_mode = part
+        elif part.strip() != "":
+            if state == None and cur_mode == rg:
+                state = rg
+            elif state == rg and cur_mode == rg:
+                pass
+            elif state == rg and cur_mode == RESET:
+                state = RESET
+            elif state == RESET and cur_mode == RESET:
+                pass
+            else:
+                return False
+    return True
+
+def convert(lines):
+    out = []
+    mode = RESET
+    in_diff = False
+    for line in lines:
+        if line.endswith('\n'):
+            line = line.replace("\n","")
+        
+        out_parts = []
+        parts = re.split("(\x1b\[[0-9]*m)", line)
+        line_modes = modes_in_line(parts, mode)
+
+        if (list(line_modes.keys()) == [] and mode in (RED, GREEN)
+            and RESET not in parts):
+            if mode == RED:
+                out_parts.append("-")
+            else:
+                out_parts.append("+")
+            out_parts.append(''.join(non_control(parts)))
+            out_parts.append("\n")
+            out.append(''.join(out_parts))
+            continue
+        
+        if list(line_modes.keys()) == [GREEN] or leading(parts, mode, GREEN):
+            if not in_diff:
+                out.append("```diff\n")
+                in_diff = True
+            out_parts.append("+")
+
+            printable = ''.join(non_control(parts))
+            if printable.strip().startswith("+"):
+                printable = printable.replace("+"," ",1)
+            out_parts.append(printable)
+            
+            for part in parts:
+                if part in (RESET, BOLD, RED, GREEN):
+                    mode = part
+            out_parts.append('\n')
+            out.append(''.join(out_parts))
+            continue
+
+        if list(line_modes.keys()) == [RED] or leading(parts, mode, RED):
+            if not in_diff:
+                out.append("```diff\n")
+                in_diff = True
+            out_parts.append("-")
+ 
+            printable = ''.join(non_control(parts))
+            if printable.strip().startswith("-"):
+                printable = printable.replace("-"," ",1)
+            out_parts.append(printable)
+            
+            for part in parts:
+                if part in (RESET, BOLD, RED, GREEN):
+                    mode = part
+            out_parts.append('\n')
+            out.append(''.join(out_parts))
+            continue
+
+        if in_diff:
+            out.append("```\n")
+            in_diff = False
+        
+        if mode == BOLD:
+            out_parts.append("**")
+        for part in parts:
+            if part in (RESET, BOLD, RED, GREEN):
+                if (mode, part) == (RESET, BOLD):
+                    out_parts.append("**")
+                elif (mode, part) == (BOLD, RESET):
+                    out_parts.append("**")
+                mode = part
+                continue
+
+            out_parts.append(part)
+        if mode == BOLD:
+            out_parts.append("**")
+        out_parts.append('\n')
+        out.append(''.join(out_parts))
+
+    if in_diff:
+        out.append("```\n")
+        
+    return out
+
+if __name__ == "__main__":
+    lines = sys.stdin.readlines()
+    out_lines = convert(lines)
+    for ol in out_lines: sys.stdout.write(ol)
+
---
+++ test_term2md.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+
+import pytest
+
+from term2md import *
+
+def test_empty():
+    assert convert([]) == []
+
+def test_single_plaintext():
+    assert convert(["a plain text line\n"]) == ["a plain text line\n"]
+
+def test_multiple_plaintext():
+    input = ["first line\n", "second line\n"]
+    assert convert(input) == input
+
+def test_full_line_bold():
+    input = ["\x1b[1ma bold choice\x1b[0m\n"]
+    assert convert(input) == ["**a bold choice**\n"]
+
+def test_inline_bold():
+    input = ["a \x1b[1mbold\x1b[0m choice\n"]
+    assert convert(input) == ["a **bold** choice\n"]
+
+def test_adjacent_bold():
+    input = ["a \x1b[1mbig, \x1b[1mbold\x1b[0m choice\n"]
+    assert convert(input) == ["a **big, bold** choice\n"]
+
+def test_bold_between_lines():
+    input = ["a \x1b[1mbig,\n", "bold\x1b[0m choice\n"]
+    assert convert(input) == ["a **big,**\n", "**bold** choice\n"]
+
+def test_ignore_spurious_resets():
+    assert convert(["\x1b[0m\n"]) == (["\n"])
+
+def test_only_green():
+    input = ["\x1b[32msome green text\x1b[0m\n"]
+    assert convert(input) == ["```diff\n", "+some green text\n", "```\n"]
+
+def test_only_red():
+    input = ["\x1b[31msome red text\x1b[0m\n"]
+    assert convert(input) == ["```diff\n", "-some red text\n", "```\n"]
+
+def test_consecutive_green():
+    input = ["\x1b[32msome green text\x1b[0m\n",
+             "\x1b[32mand more of it\x1b[0m\n"]
+    assert convert(input) == ["```diff\n", "+some green text\n",
+                              "+and more of it\n", "```\n"]
+
+def test_consecutive_red():
+    input = ["\x1b[31msome red text\x1b[0m\n",
+             "\x1b[31mand more of it\x1b[0m\n"]
+    assert convert(input) == ["```diff\n", "-some red text\n",
+                              "-and more of it\n", "```\n"]
+
+def test_mixed_red_and_green():
+    input = ["\x1b[31msome red text\x1b[0m\n",
+             "\x1b[32mand some green\x1b[0m\n"]
+    assert convert(input) == ["```diff\n", "-some red text\n",
+                              "+and some green\n", "```\n"]
+
+def test_running_green():
+    input = ["\x1b[32msome green text\n",
+             "and more of it\x1b[0m\n"]
+    assert convert(input) == ["```diff\n", "+some green text\n",
+                              "+and more of it\n", "```\n"]
+    
+def test_green_then_nondiff():
+    input = ["\x1b[32msome green text\x1b[0m\n",
+             "and some normal\n"]
+    assert convert(input) == ["```diff\n", "+some green text\n",
+                              "```\n", "and some normal\n"]
+
+def test_green_text_with_leading_plus():
+    input = ["\x1b[32m + some green text\x1b[0m\n"]
+    assert convert(input) == ["```diff\n", "+   some green text\n", "```\n"]
+
+def test_red_text_with_leading_minus():
+    input = ["\x1b[31m - some red text\x1b[0m\n"]
+    assert convert(input) == ["```diff\n", "-   some red text\n", "```\n"]
+
+def test_green_running_across_blank_line():
+    input = ["\x1b[32msome green text\n","\n","more green text\x1b[0m\n"]
+    assert convert(input) == ["```diff\n", "+some green text\n",
+                              "+\n", "+more green text\n", "```\n"]
+
+def test_red_running_across_blank_line():
+    input = ["\x1b[31msome red text\n","\n","more red text\x1b[0m\n"]
+    assert convert(input) == ["```diff\n", "-some red text\n",
+                              "-\n", "-more red text\n", "```\n"]
+
+def test_line_with_only_resets_closes_diff():
+    input = ["\x1b[31msome red text\n","\x1b[0m\n"]
+    assert convert(input) == ["```diff\n",
+                              "-some red text\n",
+                              "```\n",
+                              "\n"]
+
+def test_leading_green_can_be_green():
+    input = [" \x1b[32m+\x1b[0m plain\n"]
+    assert convert(input) == ["```diff\n",
+                              "+   plain\n",
+                              "```\n"]
+
+def test_leading_red_can_be_red():
+    input = [" \x1b[31m-\x1b[0m plain\n"]
+    assert convert(input) == ["```diff\n",
+                              "-   plain\n",
+                              "```\n"]
+    
+    
+
